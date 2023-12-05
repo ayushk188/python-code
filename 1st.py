d5dd0d82df459c20a1e7c7cff8f0b49a793519b9
ayushk188 <152911522+ayushk188@users.noreply.github.com>
2023-12-05 22:08:36 +0530

    added python file

---
+++ 1st.py
@@ -0,0 +1,108 @@
+'''
+# print("my name is ayush",end="")
+# print("my age is 18",end="")
+# print("my gender is male")
+
+# a = "ayush"
+# print(a)
+# print(type(a))
+
+# b = True
+# print(type(b))
+
+# c=5+5j
+# print(type(c))
+
+#methods
+# a = "ayush"
+# b = "aryan"
+# print(f"my name is {a}")
+# print(f"my name is {b}")
+# print(f"my name is {a} my name is {b}")
+# print("my name is",a)
+
+
+# name = input("ENTER YOUR NAME")
+# print(f"MY NAME IS = {name}")
+
+
+# n1 = int(input("enter your english no."))
+# n2 = int(input("enter your maths no."))
+# n3 = int(input("enter your phy no."))
+# n4 = int(input("enter your chem no."))
+# n5 = int(input("enter your p.e no."))
+# total = n1+n2+n3+n4+n5
+# print(total)
+
+# w=-16
+# e=5
+# print(w//e)
+'''
+
+# first_name="ayush"
+# last_name="kamboj"
+# full_name=first_name+" "+last_name
+# print("hello "+full_name)
+
+# age=21
+# age+=1
+# print("your age is:"+str(age))
+# a=2
+# a+=3
+# print(str(a))
+
+# name,course,age="ayush","bteach",18
+# print(name)
+# print(course)
+# print(age)
+
+#name="ayush kamboj"
+#print(name.upper())
+#print(name.capitalize())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("a"))
+#print(name.find("a"))
+#print(name.replace("ayush","aryan"))
+#print(name.replace("a","aa"))
+#print(len(name))
+#print(name*3)
+
+# name=input("What is your name?: ")
+# age=int(input("how old are you: "))
+# age+=1
+# print(name)
+# print(age)
+# print("i am ",age," year old")
+
+
+# import math
+# p=24.88
+# x,y,z=2,4,5
+# print(round(p))
+# print(math.ceil(p))
+# print(math.floor(p))
+# print(abs(p))
+# print(print(max(x,y,z)))
+# print(math.sqrt(25))
+
+# website="https//google.com"
+# website1="https//wikipedia.com"
+# a=slice(7,-4)
+# print(website[a])
+# print(website1[a])
+# ab="my name is ayush kamboj "
+# a=slice(11,-1)
+# print(ab[a])
+
+# temp=int(input("What is the temperature outside: "))
+# if temp>=0 and temp<=30:
+#     print("hey! ayush, the temperautre is good outside")
+# elif temp>=0 or temp<=30:
+#     print("the temperature is not good here")
+
+name=""
+while len(name)==0:
+    print(input("What is your name: "))
+print("hello",name)    
+
